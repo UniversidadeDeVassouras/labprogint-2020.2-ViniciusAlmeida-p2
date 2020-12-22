762816dfd8d5af1d86d57f4179445354161e8c48
Vinicius Almeida <vinihenriqueh@gmail.com>
2020-12-22 15:03:46 -0300

    Feat: Controller
    criado controller e finalizado a parte de produtos

--- application/controller/index_controller.py
+++ application/controller/index_controller.py
@@ -1,7 +1,17 @@
 from application import app
 from flask import render_template, request, current_app
+import json
+from application.model.entity.Produtos import Produto
 
 @app.route("/")
 def index():
     
-    return render_template('index.html')
+    with open ('application\\products.json') as product_file:
+        product_list = json.load(product_file)
+        print(product_list)
+
+    listar_produtos = []
+    for produtos in product_list:
+        listar_produtos.append(Produto(produtos['id'], produtos['image'], produtos['name'], produtos['description'], produtos['oldPrice'], produtos['price'], produtos['installments']['count'], produtos['installments']['value']))
+
+    return render_template('index.html', listar_produtos = listar_produtos)
